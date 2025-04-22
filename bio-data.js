a91477f604105f0a436283a1bd9ff47cdc2d423e
MICHAEL ANOLD TUMUHAISE <michael.tumuhaise@students.mak.ac.ug>
2025-04-22 14:43:06 +0300

    Added details on about us

---
+++ bio-data.js
@@ -0,0 +1,296 @@
+const memberData = {
+    'ddungu-henry': {
+        name: 'Dr. Ddungu Henry',
+        title: 'President',
+        image: 'img/Picture1.jpg',
+        bio: 'Dr. Ddungu graduated from Makerere University in 1998 and later a Masters (Internal Medicine) from the same University in 2003. He trained in Hematology at McMaster University, Ontario, Canada, and a has a PhD from Makerere University, Kampala, Uganda.Dr. Ddungu is a Senior Consultant (Hematology-Oncology) and Head of Division of Medical Oncology and Hematology, Uganda Cancer Institute. He is interested in global health and in advancements in the treatment of both classical and malignant hematological illnesses including immunotherapy and cellular therapies.He is the Founding President of the Hematology & Oncology Society of Uganda (HOSU).He is an Honorary Lecturer at Makerere University College of Health Sciences; Associate Clinical Professor (Adjunct), McMaster University, Faculty of Health Sciences Department of Medicine; and Honorary Lecturer, Department of Medicine, Mbarara University of Science and Technology, Uganda. For a long time, Dr. Ddungu worked with Palliative Care Organizations in Uganda and Africa, advocating for the advancement of quality palliative care to persons with life threatening illnesses. Dr. Ddungu is active in research and has been a principal investigator and co-investigator on several clinical studies. He has also supervised several Master of Medicine dissertations and has published papers in referenced journals.',
+        contact: {
+            email: 'henry.ddungu@hosu.org',
+            phone: '+256 700 123 456',
+            linkedin: 'https://linkedin.com/in/henry-ddungu'
+        }
+    },
+    'nabbosa-valeria': {
+        name: 'Dr. Nabbosa Valeria',
+        title: 'President Elect',
+        image: 'img/Picture2.jpg',
+        bio: 'Dr. Nabbosa Valeria is dedicated to advancing pediatric oncology in Uganda. She has extensive experience in clinical practice and medical education.',
+        contact: {
+            email: 'valeria.nabbosa@hosu.org',
+            phone: '+256 700 123 457'
+        }
+    },
+    'odhiambo-clara': {
+        name: 'Dr. Odhiambo Clara Atieno',
+        title: 'General Secretary',
+        image: 'img/Picture3.jpg',
+        bio: 'Clara Atieno Odhiambo is a Surgical Oncologist working with St. Francis Hospital Nsambya for the last 7 years. She graduated with a Bachelor of Medicine and Surgery from Gulu University and a Master’ in General Surgery from Uganda Martyrs University. She is a Fellow of the prestigious College of Surgeons, East Central and Southern Africa (COSECSA). She holds additional training in Surgical Oncology from India. She is the current Head of the Oncology Department at St. Francis Hospital Nsambya. Her work mainly involves offering cancer-related services such as screening, creating cancer awareness, conducting multi-disciplinary meetings that deliver patient-centric management, and performing cancer-related surgeries and palliative care. Her passion is to support cancer patients and their families at every stage of cancer.',
+        contact: {
+            email: 'clara.odhiambo@hosu.org',
+            phone: '+256 700 123 458'
+        }
+    },
+    'niyonzima-nixon': {
+        name: 'Dr. Niyonzima Nixon',
+        title: 'Treasurer',
+        image: 'img/Picture4.jpg',
+        bio: 'Dr. Niyonzima Nixon manages HOSU’s financial resources, ensuring sustainable funding for research and service delivery.',
+        contact: {
+            email: 'nixon.niyonzima@hosu.org',
+            phone: '+256 700 123 459'
+        }
+    },
+    'kakungulu-edward': {
+        name: 'Dr. Kakungulu Edward',
+        title: 'Publicity Secretary',
+        image: '/img/Picture5.jpg',
+        bio: 'I have been privileged to care for patients with cancer, offer leadership to the community of surgeons, conduct and participate in multisite research, including clinical trials, and traingraduate and postgraduate students for more than a decade.I am the President-Elect of the Association of Surgeons of Uganda, a community of over 600 surgeons of all subspecialties. I previously served as the General Secretary and, before that, as the Information Secretary and Editor-in-Chief of the same.I’m also the founding General Secretary of the Hematology and Oncology Society of Uganda(HOSU). The society brings together diverse groups of specialists and scientists to promote evidence-based practice and scientific advancements.',
+        contact: {
+            email: 'edward.kakungulu@hosu.org',
+            phone: '+256 700 123 460'
+        }
+    },
+    'namazzi-ruth': {
+        name: 'Dr. Namazzi Ruth',
+        title: 'Representative Hematology',
+        image: 'img/Picture6.jpg',
+        bio: 'Dr. Namazzi Ruth specializes in hematology and advocates for improved diagnostic services.',
+        contact: {
+            email: 'ruth.namazzi@hosu.org',
+            phone: '+256 700 123 461'
+        }
+    },
+    'akullo-anne': {
+        name: 'Dr. Akullo Anne',
+        title: 'Representative Pediatric Oncology',
+        image: 'img/Picture7.jpg',
+        bio: 'Dr. Akullo Anne focuses on pediatric oncology, working to enhance treatment options for young patients.',
+        contact: {
+            email: 'anne.akullo@hosu.org',
+            phone: '+256 700 123 462'
+        }
+    },
+    'bogere-naghib': {
+        name: 'Dr. Bogere Naghib',
+        title: 'Representative Medical Oncology',
+        image: 'img/Picture8.jpg',
+        bio: 'Dr. Bogere Naghib is an expert in medical oncology, contributing to innovative treatment protocols.',
+        contact: {
+            email: 'naghib.bogere@hosu.org',
+            phone: '+256 700 123 463'
+        }
+    },
+    'asiimwe-lois': {
+        name: 'Dr. Asiimwe Lois',
+        title: 'Representative Surgical Oncology',
+        image: 'img/Picture9.jpg',
+        bio: 'Dr Lois started her medical training at Makerere University with a Bachelor’s Degree in Medicine and Surgery. She was then selected to do Medical Internship at Mbale Regional Referral Hospital, where her love for surgery was born and nurtured. She briefly worked as a medical officer and clinical researcher before pursuing a Masters of Medicine degree in General Surgery with Uganda Martyrs University. During this time she also worked towards becoming a Member of the College of surgeons of East Central and Southern Africa (COSECSA), and later, a General Surgery Fellow by Examination. After her training she was recruited as a General Surgeon at St Francis Hospital Nsambya. However Dr Lois was always drawn to Oncology, and soon enrolled for a fellowship in Surgical Oncology at Artemis Hospitals India.She then returned to Nsambya hospital as a Surgical Oncologist at the Cancer Centre. Her roles include diagnosis and surgical treatment of cancer, as well as teaching and mentorship of various medical professionals in the areas of surgery, oncology and medical research.She has actively participated in establishing a Cancer Awareness Program that focusses on spreading knowledge about cancer prevention and treatment in different communities.Teaching and mentorship of undergraduate and postgraduate students at Kampala International University is a cause that she is passionate about, as it enables her to contribute to shaping the future of Medicine in Uganda and the rest of Africa Professional Memberships Association of Surgeons of Uganda (ASOU), College of Surgeons of East Central and Southern Africa (COSECSA), Women In Surgery Africa (WiSA), Hematology and Oncology Society of Uganda (HOSU)',
+        contact: {
+            email: 'lois.asiimwe@hosu.org',
+            phone: '+256 700 123 464'
+        }
+    },
+    'kibudde-solomon': {
+        name: 'Dr. Kibudde Solomon',
+        title: 'Representative Radiation Oncology',
+        image: 'img/Picture10.jpg',
+        bio: 'Dr. Kibudde Solomon advances radiation therapy techniques for effective cancer treatment.',
+        contact: {
+            email: 'solomon.kibudde@hosu.org',
+            phone: '+256 700 123 465'
+        }
+    },
+    'ssali-francis': {
+        name: 'Dr. Ssali Francis',
+        title: 'Representative Research',
+        image: 'img/Picture11.jpg',
+        bio: 'I have over three decades of experience as a Physician specializing in Internal Medicine and Hematology. I am currently the Deputy Executive Director for Research and Clinical Services at the Joint Clinical Research Centre (JCRC) in Kampala Uganda, where I have worked for the past 16 years providing care and treatment for People Living with HIV, Sickle Cell Disease (SCD) and other medical conditions. The development of cell and gene therapy products especially for HIV and SCD in the case of Uganda is critical due to the high disease burden coupled with limited resources and the need to develop the requisite human resource such as scientists and clinicians is the goal of our proposal. I will serve as part of the faculty for this program based on my extensive experience as a clinician and researcher. I am an Investigator on various studies including the ACTG–Network studies, HIV reservoir studies, pediatric dose-finding studies on Rilpivirine, studies investigating the effect of antiretroviral therapy on lymphatic tissue among PLHIV in Uganda, and protocols in development for cell and gene therapy trials for both HIV and SCD. I hold Faculty positions at the Makerere University College of Health Sciences where I instruct medical residents in HIV Medicine and Hematology as well as at the Uganda Cancer Institute. I lead the therapeutic apheresis team at the JCRC and I am a member of the Global Gene Therapy Initiative (GGTI) which is an alliance of various experts working towards the development of and enabling access to cell and gene therapy products in low and middle-income countries such as Uganda.',
+        contact: {
+            email: 'francis.ssali@hosu.org',
+            phone: '+256 700 123 466'
+        }
+    },
+    'lukande-robert': {
+        name: 'Dr. Lukande Robert',
+        title: 'Representative Pathology',
+        image: 'img/Picture12.jpg',
+        bio: 'Dr. Lukande Robert is a pathologist dedicated to improving cancer diagnostics.',
+        contact: {
+            email: 'robert.lukande@hosu.org',
+            phone: '+256 700 123 467'
+        }
+    },
+    'kadhumbula-sylvester': {
+        name: 'Dr. Kadhumbula Sylvester',
+        title: 'Representative Laboratory',
+        image: 'img/Picture13.jpg',
+        bio: 'MSc Haematology and blood transfusion- Mbarara University of Science and Technology Principal Medical Lab Scientist/ Laboratory Manager Uganda Cancer Institute',
+        contact: {
+            email: 'sylvester.kadhumbula@hosu.org',
+            phone: '+256 700 123 468'
+        }
+    },
+    'irumba-lisa': {
+        name: 'Ms. Irumba Lisa',
+        title: 'Representative Palliative Care',
+        image: 'img/Picture14.jpg',
+        bio: 'Lisa Christine Irumba is a Palliative Care Clinician, Pain Specialist and Research and Information Manager at the Palliative Care Association of Uganda. She coordinates advocacy, awareness initiatives, and research and information at the association. Her work emphasises the need for evidence, equity and social justice in health care.',
+        contact: {
+            email: 'lisa.irumba@hosu.org',
+            phone: '+256 700 123 469'
+        }
+    },
+    'moses-echodu': {
+        name: 'Mr. Moses Echodu',
+        title: 'Representative Civil Society Organizations',
+        image: 'img/Picture15.jpg',
+        bio: 'Mr. Moses Echodu bridges HOSU with civil society to enhance community outreach.',
+        contact: {
+            email: 'moses.echodu@hosu.org',
+            phone: '+256 700 123 470'
+        }
+    }
+    
+};
+
+// Mobile menu toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', (e) => {
+    e.stopPropagation();
+    navLinks.classList.toggle('active');
+});
+
+// Close mobile menu when clicking outside
+document.addEventListener('click', (e) => {
+    if (!e.target.closest('.nav-links') && !e.target.closest('.menu-toggle')) {
+        navLinks.classList.remove('active');
+    }
+});
+
+// Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+            
+            // Close mobile menu if open
+            navLinks.classList.remove('active');
+        }
+    });
+});
+
+// Member carousel functionality
+let currentSlide = 0;
+const memberSlides = document.querySelectorAll('.member-slide');
+const carouselDots = document.querySelectorAll('.carousel-dot');
+const prevButton = document.querySelector('.carousel-arrow.prev');
+const nextButton = document.querySelector('.carousel-arrow.next');
+
+function showSlide(index) {
+    memberSlides.forEach(slide => slide.classList.remove('active'));
+    carouselDots.forEach(dot => dot.classList.remove('active'));
+    
+    memberSlides[index].classList.add('active');
+    carouselDots[index].classList.add('active');
+    currentSlide = index;
+}
+
+function nextSlide() {
+    const next = (currentSlide + 1) % memberSlides.length;
+    showSlide(next);
+}
+
+function prevSlide() {
+    const prev = (currentSlide - 1 + memberSlides.length) % memberSlides.length;
+    showSlide(prev);
+}
+
+// Set up event listeners for arrow buttons
+prevButton.addEventListener('click', prevSlide);
+nextButton.addEventListener('click', nextSlide);
+
+// Click events for dots
+carouselDots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        showSlide(index);
+    });
+});
+
+// Member details overlay functionality
+function showMemberDetails(memberId) {
+    const overlay = document.getElementById('member-overlay');
+    const data = memberData[memberId];
+
+    if (data) {
+        // Populate overlay with member data
+        document.getElementById('detail-image').src = data.image;
+        document.getElementById('detail-image').alt = data.name;
+        document.getElementById('detail-title').textContent = data.title;
+        document.getElementById('detail-name').textContent = data.name;
+        document.getElementById('detail-bio').innerHTML = `<p>${data.bio}</p>`;
+
+        // Populate contact info
+        const contactContainer = document.getElementById('detail-contact');
+        contactContainer.innerHTML = '';
+        if (data.contact.email) {
+            contactContainer.innerHTML += `
+                <div class="contact-item">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                    </svg>
+                    <a href="mailto:${data.contact.email}">${data.contact.email}</a>
+                </div>
+            `;
+        }
+        if (data.contact.phone) {
+            contactContainer.innerHTML += `
+                <div class="contact-item">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                        <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 0-2.612.163L.174 1.773c-.604.604-.749 1.553-.403 2.294a18.69 18.69 0 0 0 4.433 7.02 18.691 18.691 0 0 0 7.02 4.433c.741.346 1.69.201 2.294-.403l1.098-1.098a1.745 1.745 0 0 0 .163-2.612l-2.307-1.794a1.745 1.745 0 0 0-1.494-.315l-2.19.547a.678.678 0 0 1-.645-.179L5.482 7.225a.678.678 0 0 1-.179-.645l.548-2.19a1.745 1.745 0 0 0-.315-1.494L1.884.511z"/>
+                    </svg>
+                    <a href="tel:${data.contact.phone}">${data.contact.phone}</a>
+                </div>
+            `;
+        }
+        if (data.contact.linkedin) {
+            contactContainer.innerHTML += `
+                <div class="contact-item">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                    </svg>
+                    <a href="${data.contact.linkedin}" target="_blank">LinkedIn Profile</a>
+                </div>
+            `;
+        }
+    } else {
+        // Fallback if no data is found
+        document.getElementById('detail-image').src = '/placeholder.svg?height=150&width=150';
+        document.getElementById('detail-title').textContent = 'Unknown Position';
+        document.getElementById('detail-name').textContent = 'Unknown Member';
+        document.getElementById('detail-bio').innerHTML = '<p>No information available.</p>';
+        document.getElementById('detail-contact').innerHTML = '';
+    }
+
+    overlay.classList.add('active');
+}
+
+function closeMemberDetails() {
+    const overlay = document.getElementById('member-overlay');
+    overlay.classList.remove('active');
+}
+
+// Close overlay when clicking outside the details card
+document.getElementById('member-overlay').addEventListener('click', function(e) {
+    if (e.target === this) {
+        closeMemberDetails();
+    }
+});
